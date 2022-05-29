e1488e33948eb0fb9c2e3e86ea441cfa25a5c17f
Jack Dickson <98648241+JackOfDickson@users.noreply.github.com>
2022-05-29 11:16:26 +0100

    Health Bar
    Added health bar

--- Hell_Crusade/Assets/Scripts/PlayerHealth.cs
+++ Hell_Crusade/Assets/Scripts/PlayerHealth.cs
@@ -7,12 +7,17 @@ public class PlayerHealth : MonoBehaviour
     public int maxHealth;
     public int currentHealth;
 
+    //This is for a health bar that appears on the UI
+    public HealthBarScript healthBar;
+
     public void ReceiveDamage(int attackDamage){
         currentHealth -= attackDamage;
+        healthBar.SetHealth(currentHealth);
     }
 
     void Start(){
         currentHealth = maxHealth;
+        healthBar.MaxHealth(maxHealth);
     }
 
 }
