72f55fdcdea6737333f1aadd7a241276fbe93a1e
Matt-Matthaiou <Mat.matthaiou@gmail.com>
2022-05-29 15:41:11 +0100

    Added enemy targeting and bullet damage to player.

---
+++ Hell_Crusade/Assets/Scripts/EnemyBullet.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+
+    PlayerHealth playerHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col){
+        if(col.gameObject.tag.Equals("Player"))
+        {
+            playerHealth = col.gameObject.GetComponent<PlayerHealth>();
+            playerHealth.ReceiveDamage(5);
+            Destroy(gameObject);
+
+        }
+    }
+}
---
+++ Hell_Crusade/Assets/Scripts/EnemyShoot.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShoot : MonoBehaviour
+{
+
+    public Transform playerPosition;
+    public float projectileForce = 5f;
+    public Transform projexit;
+    private float coolDown = 5f;
+    private float activeCooldown;
+    private Vector3 rayDir;
+    public GameObject bloodProjectile;
+    // Start is called before the first frame update
+    void Start()
+    {
+        activeCooldown = 0f;
+    }
+
+    // Update is called once per frame
+   void Update()
+    {   
+        
+        rayDir = transform.TransformDirection(playerPosition.transform.position - transform.position);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, rayDir);
+        if(hit.collider.tag == "Player"){
+            Shoot();
+        }
+
+        if(activeCooldown > 0){
+            activeCooldown -= Time.deltaTime;
+        }
+        Debug.Log(activeCooldown);
+        
+    }
+
+    void Shoot(){
+        if(activeCooldown <= 0){
+            GameObject bullet = Instantiate(bloodProjectile, projexit.position, projexit.rotation);
+            Rigidbody2D body = bullet.GetComponent<Rigidbody2D>();
+            body.AddForce(rayDir * projectileForce, ForceMode2D.Impulse);
+            activeCooldown = coolDown;
+        }
+    }
+}
