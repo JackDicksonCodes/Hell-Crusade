762eb7441b78dad67816d922d82813f1f050d34a
Jack Dickson <jackowendickson@gmail.com>
2022-05-31 12:07:26 +0100

    Pause menu
    created a prefab for playerHUDv1.1 and button to main menu on pause menu

--- Hell_Crusade/Assets/Scripts/UI/Menus/PauseMenu.cs
+++ Hell_Crusade/Assets/Scripts/UI/Menus/PauseMenu.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -31,5 +32,9 @@ public class PauseMenu : MonoBehaviour
         Time.timeScale = 0f;
         pauseMenu.SetActive(true);
         isPaused = true;
+    }
+
+    public void toMainMenu(){
+        SceneManager.LoadScene("Main Menu");
     }
 }
