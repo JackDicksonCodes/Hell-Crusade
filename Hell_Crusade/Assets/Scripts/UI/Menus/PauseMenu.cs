d215c8e56305e99b0ee5b751cf846f9eeb04ee8a
Jack Dickson <jackowendickson@gmail.com>
2022-05-31 11:31:35 +0100

    Pause menu works but hover is not working

---
+++ Hell_Crusade/Assets/Scripts/UI/Menus/PauseMenu.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isPaused = false;
+
+    public GameObject pauseMenu;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            if (isPaused == true){
+                resume();
+            } else {
+                pause();
+            }
+        }
+    }
+
+    public void resume(){
+        Time.timeScale = 1f;
+        pauseMenu.SetActive(false);
+        isPaused = false;
+    }
+
+    public void pause(){
+        Time.timeScale = 0f;
+        pauseMenu.SetActive(true);
+        isPaused = true;
+    }
+}
