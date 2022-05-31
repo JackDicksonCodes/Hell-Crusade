28620a8d2b43d24bda9b175efd18b8df856777ab
Jack Dickson <jackowendickson@gmail.com>
2022-05-31 14:51:16 +0100

    Made a door in a test scene that returns player to main menu

--- Hell_Crusade/Assets/Scripts/Level/StageDoor.cs
+++ Hell_Crusade/Assets/Scripts/Level/StageDoor.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StageDoor : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public string nextSceneName;
+
+
+    public void MoveToNextStage(){
+        SceneManager.LoadScene(nextSceneName);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    void OnTriggerEnter2D(Collider2D col){
+        if(col.gameObject.tag.Equals("Player")){
+            Debug.Log("hit door");
+            MoveToNextStage();
+            }
+        }
 }
