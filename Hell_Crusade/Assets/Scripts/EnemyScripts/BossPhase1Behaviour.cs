b43b8e70cf47de0b99ec5bcf9734f82bb50244b9
Matt-Matthaiou <Mat.matthaiou@gmail.com>
2022-06-06 14:42:58 +0100

    boss phase 1 completed

--- Hell_Crusade/Assets/Scripts/EnemyScripts/BossPhase1Behaviour.cs
+++ Hell_Crusade/Assets/Scripts/EnemyScripts/BossPhase1Behaviour.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class BossPhase1Behaviour : MonoBehaviour
 {
     public bool phase1Active;
@@ -15,6 +16,9 @@ public class BossPhase1Behaviour : MonoBehaviour
     public GameObject bloodProjectile;
     private Vector3 rayDir;
     public float coolDown = 1f;
+    private int attackChoice;
+    public float specialProjectileForce;
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -26,11 +30,13 @@ public class BossPhase1Behaviour : MonoBehaviour
         destPoint = 0;
         agent.updateUpAxis = false;
         agent.updateRotation = false;
+    
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+
         if(phase1Active){
             if(agent.remainingDistance == 0){
                     GotoNextPoint();
@@ -58,8 +64,19 @@ public class BossPhase1Behaviour : MonoBehaviour
         rayDir = transform.TransformDirection(player.transform.position - transform.position);
         RaycastHit2D hit = Physics2D.Raycast(transform.position, rayDir);
         if(activeCooldown <= 0){
-            StartCoroutine(ShootSequence());
             activeCooldown = coolDown;
+            attackChoice = Random.Range(1, 101);
+            switch(attackChoice){
+            case int i when i > 1 && i <= 40:
+                StartCoroutine(ShootSequence());
+                break;
+            case int i when i > 40 && i <= 80:
+                StartCoroutine(ShootTriangleSequence());
+                break;
+            case int i when i > 80 && i <= 100:
+                StartCoroutine(ShootAroundSequence());
+                break;
+            }
         }
 
         if(activeCooldown > 0){
@@ -76,12 +93,66 @@ public class BossPhase1Behaviour : MonoBehaviour
         }
     }
 
-    private void Shoot(){
+    IEnumerator ShootTriangleSequence(){
+        int i = 0;
+        while(i < 3){
+            yield return new WaitForSeconds(0.5f);
+            ShootTriangle();
+            i ++;
+        }
+    }
+
+    IEnumerator ShootAroundSequence(){
+        int i = 0;
+        while( i < 8 ){
+            yield return new WaitForSeconds(0.5f);
+            ShootAround();
+            i ++;
+            transform.Rotate(0,0,45);
+        }
+    }
+
+    private void Shoot()
+    {
         
         GameObject bullet = Instantiate(bloodProjectile, projexit.position, projexit.rotation);
         Rigidbody2D body = bullet.GetComponent<Rigidbody2D>();
         body.AddForce(rayDir * projectileForce, ForceMode2D.Impulse);
         activeCooldown = coolDown;
         
+    }
+
+    private void ShootTriangle()
+    {
+        
+        Vector3 rayDir2 = transform.TransformDirection(rayDir + new Vector3(5,5,0));
+        Vector3 rayDir3 = transform.TransformDirection(rayDir - new Vector3(5,5,0));
+        GameObject bullet = Instantiate(bloodProjectile, projexit.position, projexit.rotation);
+        GameObject bullet2 = Instantiate(bloodProjectile, projexit.position, projexit.rotation);
+        GameObject bullet3 = Instantiate(bloodProjectile, projexit.position, projexit.rotation);
+        Rigidbody2D body = bullet.GetComponent<Rigidbody2D>();
+        body.AddForce(rayDir * projectileForce, ForceMode2D.Impulse);
+        Rigidbody2D body2 = bullet2.GetComponent<Rigidbody2D>();
+        body2.AddForce(rayDir2 * projectileForce, ForceMode2D.Impulse);
+        Rigidbody2D body3 = bullet3.GetComponent<Rigidbody2D>();
+        body3.AddForce(rayDir3 * projectileForce, ForceMode2D.Impulse);
+        activeCooldown = coolDown;
+    }
+
+    private void ShootAround(){
+        GameObject bullet = Instantiate(bloodProjectile, projexit.position, projexit.rotation);
+        GameObject bullet2 = Instantiate(bloodProjectile, projexit.position, projexit.rotation);
+        GameObject bullet3 = Instantiate(bloodProjectile, projexit.position, projexit.rotation);
+        GameObject bullet4 = Instantiate(bloodProjectile, projexit.position, projexit.rotation);
+        Rigidbody2D body = bullet.GetComponent<Rigidbody2D>();
+        body.AddForce(projexit.up * specialProjectileForce, ForceMode2D.Impulse);
+        Rigidbody2D body2 = bullet2.GetComponent<Rigidbody2D>();
+        body2.AddForce(-projexit.up * specialProjectileForce, ForceMode2D.Impulse);
+        Rigidbody2D body3 = bullet3.GetComponent<Rigidbody2D>();
+        body3.AddForce(-projexit.right * specialProjectileForce, ForceMode2D.Impulse);
+        Rigidbody2D body4 = bullet4.GetComponent<Rigidbody2D>();
+        body4.AddForce(projexit.right * specialProjectileForce, ForceMode2D.Impulse);
+        activeCooldown = coolDown;
+
     }
 }
